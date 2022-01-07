781a6c1160930d9d5afb6adaea8bc7cd026d8450
devilWH40k <devilwork123123@gmail.com>
2022-01-07 22:44:08 +0200

    created html files for future using

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -5,13 +5,28 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route("/")
 @app.route("/home")
-def home():
-    return "<h1>Test run</h1>"
-
-
-@app.route("/test")
-def test():
+def departments():
     return render_template('departments.html')
+
+
+@app.route("/department")
+def department():
+    return render_template('department.html')
+
+
+@app.route("/employees")
+def employees():
+    return render_template('employees.html')
+
+
+@app.route("/employee")
+def employee():
+    return render_template('employee.html')
+
+
+@app.route("/manage")
+def manage():
+    return render_template('manage.html')
 
 
 if __name__ == '__main__':
