13747093fd7ac416a425ebd9c7bbb0a469458fd3
devilWH40k <devilwork123123@gmail.com>
2022-01-08 15:53:25 +0200

    modified department pages and views

--- department_app/views/views.py
+++ department_app/views/views.py
@@ -1,17 +1,24 @@
 from department_app import app
-from flask import render_template
+from flask import render_template, url_for
 from department_app.models import Department, Employee
 
 
 @app.route("/")
 @app.route("/departments")
-def departments():
-    return render_template('departments.html')
+def show_departments():
+    """Main page with all departments in db."""
+    try:
+        departments = Department.query.all()
+    except:
+        departments = None
+    return render_template('departments.html', departments=departments)
 
 
-@app.route("/department")
-def department():
-    return render_template('department.html')
+@app.route("/departments/<dep_id>")
+def show_department(dep_id):
+    """Department page with its full information."""
+    department = Department.query.get_or_404(dep_id)
+    return render_template('department.html', department=department)
 
 
 @app.route("/employees")
