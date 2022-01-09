c7468d9da0b09ad5b30430684181d1aa3f298d76
devilWH40k <devilwork123123@gmail.com>
2022-01-10 01:43:25 +0200

    delete routes and flash messages

--- department_app/views/views.py
+++ department_app/views/views.py
@@ -1,5 +1,5 @@
-from department_app import app
-from flask import render_template, url_for
+from department_app import app, db
+from flask import render_template, redirect, flash, url_for
 from department_app.models import Department, Employee
 
 
@@ -21,8 +21,23 @@ def show_department(dep_id):
     return render_template('department.html', department=department)
 
 
+@app.route("/departments/delete/<dep_id>")
+def delete_department(dep_id):
+    """Route for deleting the department and all related employees by department id."""
+    department = Department.query.get_or_404(dep_id)
+    for employee in department.employees:
+        db.session.delete(employee)
+    db.session.delete(department)
+    db.session.commit()
+    flash(f'{department.name} was successfully deleted!', category='success')
+
+    # redirecting to main page with departments after deleting
+    return redirect(url_for('show_departments'))
+
+
 @app.route("/employees/<dep_id>")
 def show_employees(dep_id):
+    """Route for page with employees from chosen department."""
     department = Department.query.get_or_404(dep_id)
     employees = department.employees
     return render_template('employees.html', employees=employees, dep_name=department.name)
@@ -32,6 +47,18 @@ def show_employees(dep_id):
 def show_employee(emp_id):
     employee = Employee.query.get_or_404(emp_id)
     return render_template('employee.html', employee=employee)
+
+
+@app.route("/employee/delete/<emp_id>")
+def delete_employee(emp_id):
+    """Route for deleting employee by his id."""
+    employee = Employee.query.get_or_404(emp_id)
+    db.session.delete(employee)
+    db.session.commit()
+    flash(f'{employee.name} {employee.surname[0]}. was successfully deleted!', category='success')
+
+    # redirecting to the employees page
+    return redirect(url_for('show_employees', dep_id=employee.dep_id))
 
 
 @app.route("/manage")
