e118234b614a6c50d717e1b74313a4859ea5334f
devilWH40k <devilwork123123@gmail.com>
2022-01-10 21:41:37 +0200

    wtf forms and adding data to db by user

---
+++ department_app/forms.py
@@ -0,0 +1,45 @@
+"""Module for storing web form classes."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, SelectField, DateField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, ValidationError
+from department_app.models import Department, Employee
+
+
+class DepartmentForm(FlaskForm):
+    """Class that represents the Department form.
+
+    Checks on the name uniqueness.
+    """
+
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=25)])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    submit = SubmitField('Create')
+
+    def validate_name(self, name):
+        validate_name = Department.query.filter_by(name=name.data).first()
+        if validate_name:
+            raise ValidationError('That department name is taken. Please choose a different one.')
+
+
+class EmployeeForm(FlaskForm):
+    """Class that represents the Employee form.
+
+    Checks on the email uniqueness.
+    """
+
+    dep_id = SelectField('Works for?',
+                         choices=[(dep.id, dep.name) for dep in Department.query.all()])
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=25)])
+    surname = StringField('Surname', validators=[DataRequired(), Length(min=2, max=25)])
+    email = StringField('Email address', validators=[DataRequired(), Email()])
+    brief_inf = TextAreaField('Brief resume', validators=[DataRequired()])
+    birth_date = DateField('Birth date')
+    salary = IntegerField('Salary', validators=[DataRequired()])
+    submit = SubmitField('Create')
+
+    def validate_email(self, email):
+        validate_email = Employee.query.filter_by(email=email.data).first()
+        if validate_email:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+
--- department_app/views/views.py
+++ department_app/views/views.py
@@ -1,16 +1,15 @@
+"""Views module for app routing logic."""
 from department_app import app, db
 from flask import render_template, redirect, flash, url_for
 from department_app.models import Department, Employee
+from department_app.forms import DepartmentForm, EmployeeForm
 
 
 @app.route("/")
 @app.route("/departments")
 def show_departments():
     """Main page with all departments in db."""
-    try:
-        departments = Department.query.all()
-    except:
-        departments = None
+    departments = Department.query.all()
     return render_template('departments.html', departments=departments)
 
 
@@ -23,7 +22,10 @@ def show_department(dep_id):
 
 @app.route("/departments/delete/<dep_id>")
 def delete_department(dep_id):
-    """Route for deleting the department and all related employees by department id."""
+    """Route for deleting the department and all related employees by department id.
+
+    Redirects to departments page after deleting.
+    """
     department = Department.query.get_or_404(dep_id)
     for employee in department.employees:
         db.session.delete(employee)
@@ -45,13 +47,17 @@ def show_employees(dep_id):
 
 @app.route("/employee/<emp_id>")
 def show_employee(emp_id):
+    """Route for employee page got by his id."""
     employee = Employee.query.get_or_404(emp_id)
     return render_template('employee.html', employee=employee)
 
 
 @app.route("/employee/delete/<emp_id>")
 def delete_employee(emp_id):
-    """Route for deleting employee by his id."""
+    """Route for deleting employee by his id.
+
+    Redirects to employees page after deleting.
+    """
     employee = Employee.query.get_or_404(emp_id)
     db.session.delete(employee)
     db.session.commit()
@@ -61,6 +67,37 @@ def delete_employee(emp_id):
     return redirect(url_for('show_employees', dep_id=employee.dep_id))
 
 
-@app.route("/manage")
+@app.route("/manage", methods=['POST', 'GET'])
 def manage():
-    return render_template('manage.html')
+    """Page with forms for adding departments and employees to database.
+
+    After POST request from the department's form redirects to departments page.
+    In case with the employee's form redirects to employees page.
+    """
+    dep_form = DepartmentForm()
+    emp_form = EmployeeForm()
+    if dep_form.validate_on_submit():
+        department = Department(
+            name=dep_form.name.data,
+            description=dep_form.description.data
+        )
+        db.session.add(department)
+        db.session.commit()
+        flash(f'{department.name} was successfully created!', category='success')
+        return redirect(url_for('show_departments'))
+    if emp_form.validate_on_submit():
+        employee = Employee(
+            name=emp_form.name.data,
+            surname=emp_form.surname.data,
+            email=emp_form.email.data,
+            brief_inf=emp_form.brief_inf.data,
+            birth_date=emp_form.birth_date.data,
+            salary=emp_form.salary.data,
+            dep_id=emp_form.dep_id.data
+        )
+        db.session.add(employee)
+        db.session.commit()
+        flash(f'{employee.name} {employee.surname[0]}. was successfully added to {employee.department.name}!',
+              category='success')
+        return redirect(url_for('show_employees', dep_id=employee.dep_id))
+    return render_template('manage.html', dep_form=dep_form, emp_form=emp_form)
