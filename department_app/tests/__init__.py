44c1454dd639419e471d7e37c24b31ffbd9459a2
devilWH40k <devilwork123123@gmail.com>
2022-01-16 15:17:59 +0200

    updated __init__ file of tests package

--- department_app/tests/__init__.py
+++ department_app/tests/__init__.py
@@ -1,6 +1,3 @@
-"""__init__ module of 'tests' package with imported test Classes."""
+"""__init__ module of 'tests' package."""
 
-from .test_department_model import TestDepartmentModel
-from .test_employee_model import TestEmployeeModel
-from .test_views import TestViews
-from .test_department_service import TestDepartmentService
+
